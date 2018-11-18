899ff96a50a906551f075e5757b6c7bcd7cb588d
terrygonguet <terry@gonguet.com>
2018-11-18 14:05:10 +0100

    Added minifying and hash naming of build files

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,20 @@
 const merge = require("webpack-merge")
 const common = require("./webpack.common")
+const MinifyPlugin = require("babel-minify-webpack-plugin")
+const path = require("path")
 
 module.exports = merge(common, {
+	output: {
+		filename: "[hash].bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
 	mode: "production",
 	devtool: "source-map",
 	optimization: {
 		splitChunks: {
 			chunks: "all",
 		},
+		moduleIds: "hashed",
 	},
+	plugins: [new MinifyPlugin()],
 })
